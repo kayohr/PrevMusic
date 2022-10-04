8eaaa60cd33bb16a8a228d7f3c624bdcecb42bad
kayo <kayohramos@gmail.com>
2022-10-04 15:15:25 -0300

    1-4

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 
 class Header extends React.Component {
@@ -30,6 +31,23 @@ class Header extends React.Component {
             ? <p>Carregando...</p>
             : <p data-testid="header-user-name">{loginName}</p>
         }
+        <nav>
+          <NavLink
+            data-testid="link-to-search"
+            to="/search"
+            activeClassName="active"
+          />
+          <NavLink
+            data-testid="link-to-favorites"
+            to="/favorites"
+            activeClassName="active"
+          />
+          <NavLink
+            data-testid="link-to-profile"
+            to="/profile"
+            activeClassName="active"
+          />
+        </nav>
       </header>
     );
   }
