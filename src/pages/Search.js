44e335f09d40712bc5b71f1efa9f2f40a1783b41
kayo <kayohramos@gmail.com>
2022-10-04 17:48:05 -0300

    1-5 atualizado

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -9,16 +9,15 @@ class Search extends React.Component {
     this.state = {
       // sizeIdex: 0,
       isSaveButtonDisabled: true,
-      searchName: '',
       // load: false,
     };
   }
 
   fucMaior2 = (event) => {
-    const { searchName } = this.state;
+    // const { searchName } = this.state;
     if (event.target.value.length >= character) {
-      this.setState({ isSaveButtonDisabled: false, searchName: event.target.value });
-    } this.setState({ searchName: event.target.value });
+      this.setState({ isSaveButtonDisabled: false });
+    }
   };
 
   // validatonButton = async () => {
@@ -35,21 +34,21 @@ class Search extends React.Component {
       <div data-testid="page-search">
         Search
         <Header />
-        <form>
-          <input
-            data-testid="search-artist-input"
-            placeholder="usernameArtista"
-            onChange={ this.fucMaior2 }
-          />
-          <button
-            type="button"
-            data-testid="search-artist-button"
-            disabled={ isSaveButtonDisabled }
-            onClick="{ }"
-          >
-            Pesquisar
-          </button>
-        </form>
+
+        <input
+          data-testid="search-artist-input"
+          placeholder="usernameArtista"
+          onChange={ this.fucMaior2 }
+          type="text"
+        />
+        <button
+          type="button"
+          data-testid="search-artist-button"
+          disabled={ isSaveButtonDisabled }
+        >
+          Pesquisar
+        </button>
+
       </div>
     );
   }
